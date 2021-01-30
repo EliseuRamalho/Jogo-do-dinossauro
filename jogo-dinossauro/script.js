321084b4fa94699abc619c94b566c21da91c45c4
EliseuRamalho <zeu.ramalho@hotmail.com>
2021-01-30 20:24:39 -0300

    Criando a função de fazer o dinossauro pular e retornar ao estado inicial.
    A função Jump faz com que o dinossauro pule pra cima e retorne para baixo a cada 20 mls.

--- jogo-dinossauro/script.js
+++ jogo-dinossauro/script.js
@@ -4,8 +4,32 @@ const dino = document.querySelector('.dino');
 
 function handleKeyUp(Event) {
     if(Event.keyCode === 32){
-        console.log('A tecla espaço foi clicada!');
+        jump();
     }
+}
+
+function jump() {
+    let  position = 0;
+
+    let upInterval = setInterval(() => {
+        if (position >= 150) {
+            clearInterval(upInterval);
+    
+    // Dinossauro Descendo
+    let downInterval = setInterval(() => {
+        if (position <= 0) {
+            clearInterval(downInterval);
+        } else {
+            position -= 20;
+            dino.style.bottom = position + 'px';
+            }
+        }, 20);
+    } else {
+    // Dinossauro Subindo
+        position += 20;
+        dino.style.bottom = position + 'px';
+        }
+    }, 20);
 }
 
 document.addEventListener('keyup', handleKeyUp);
